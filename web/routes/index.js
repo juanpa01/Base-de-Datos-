d6a98dccab59cefb44754f741d9c05eef0f809de
juanpa01 <juanpablo961115@hotmail.com>
2017-08-11 15:53:04 -0500

    Proyecto terminado

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var dialog = require('dialog');
+var validator = require('validator');
 
 
 /*------------ DATABASE CONNECTION ----------------*/
 var connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : 'ander1096',
+  password : 'juan9611',
   database : 'C_infracciones'
 });
 
@@ -45,6 +47,42 @@ router.get('/consult', function(req, res, next) {
   res.render('consult');
 });
 
+
+/* GET root page */
+router.get('/root', function(req, res, next) {
+  res.render('root');
+});
+
+// GET register_mult page
+router.get('/register_mult', function(req, res, next){
+  res.render('register_mult');
+});
+
+// GET delete_person page
+router.get('/delete_person', function(req, res, next){
+  consult = 'SELECT * FROM Persona';
+  connection.query(consult,function(err,result){
+    if(err){
+      throw err;
+    }
+    res.render('delete_person', {person: result});
+  });
+});
+
+
+// GET update_person page
+router.get('/update_person', function(req, res, next){
+  consult = 'SELECT * FROM Persona';
+  connection.query(consult,function(err,result){
+    if(err){
+      throw err;
+    }
+    res.render('update_person', {person: result});
+  });
+});
+
+
+
 /*--------------------------------------------------------*/
 
 
@@ -54,9 +92,12 @@ router.post('/login',function(req,res,next){
   try{
     var obj = req.body;
     if(obj.login_pass == 'root' && obj.login_user == 'root'){
-      res.redirect('/');
+      res.redirect('/root')
     }else{
-      res.redirect('/');
+      dialog.info('Usuario incorrecto', 'Error!', function(err){
+        if (!err) console.log('User clicked OK');
+      })
+      res.render('login')
     }
   }
   catch(ex){
@@ -109,6 +150,180 @@ router.post('/consult',function(req,res,next){
     console.error("Internal error:" + ex);
     return next(ex);
   }
+});
+
+
+//Actualizar datos persona
+router.post('/update_person',function(req,res,next){
+  try {
+    var obj = req.body;
+    if (validator.isNumeric(obj.id)){
+      var id = obj.id;
+      if (obj.columna == 'Identificacion'){
+        var data = {id_persona: obj.inf};
+        var possible = true;
+      }
+      if (obj.columna == 'Nombre'){
+        var data = {nombre_persona: obj.inf};
+        var possible = true;
+      }
+      if (obj.columna == 'Apellido'){
+        var data = {apellido_persona: obj.inf};
+        var possible = true;
+      }
+      if (obj.columna == 'Direccion'){
+        var data = {direccion_persona: obj.inf};
+        var possible = true;
+      }
+      if (obj.columna == 'Fecha de nacimiento'){
+        var data = {fecha_nacimiento: obj.inf};
+        var possible = true;
+      }
+      if (obj.columna == 'Tipo'){
+        var data = {tipo_persona: obj.inf};
+        var possible = true;
+      }
+
+      if (possible == true){
+        up = 'UPDATE Persona SET ? WHERE id_persona = ?';
+        connection.query(up, [data, id],function(err,result){
+          if(err){
+            throw err;
+          }
+          console.log("se realizo la actualizacion");
+        });
+      }
+    }else {
+      console.log("error en identificacion");
+    }
+    //vuelve a cargar la pagina con datos actualizados
+    consult = 'SELECT * FROM Persona';
+    connection.query(consult,function(err,result){
+      if(err){
+        throw err;
+      }
+      res.render('update_person', {person: result});
+    });
+
+  } catch (e) {
+    console.error("Internal error:" + ex);
+    return next(ex);
+  }
+});
+
+
+//Eliminar Multa
+router.post('/delete_person',function(req,res,next){
+  try {
+    var obj = req.body;
+    if (validator.isNumeric(obj.id)){
+      delet = 'DELETE FROM Persona WHERE id_persona = ?';
+      connection.query(delet,obj.id,function(err, res){
+        if(err){
+          throw err;
+        }
+      });
+    } else {
+      console.log("error en identificacion");
+    }
+    //vuelve a cargar la pagina con datos actualizados
+    consult = 'SELECT * FROM Persona';
+    connection.query(consult,function(err,result){
+      if(err){
+        throw err;
+      }
+      res.render('delete_person', {person: result});
+    });
+  } catch (ex) {
+    console.error("Internal error:" + ex);
+    return next(ex);
+  }
+
+
+});
+
+
+//Ingresar multa
+router.post('/register_mult',function(req,res,next){
+  try {
+    var obj = req.body;
+
+    if (validator.isNumeric(obj.id) && validator.isNumeric(obj.id_agent) && obj.person != null && obj.vehiculo != null){
+      if (obj.person == 'no' && validator.isNumeric(obj.id)){
+        var id = obj.id.replace(/[^0-9]/g, '');
+        var data_person = {id_persona: id, nombre_persona: obj.name, apellido_persona: obj.last_name, direccion_persona: obj.dir, fecha_nacimiento: obj.date_person, tipo_persona: obj.tipo};
+        ins = 'INSERT INTO Persona SET ?';
+        connection.query(ins,data_person,function(err, res){
+          if(err){
+            throw err;
+          }
+          console.log("insercion completa en persona");
+        });
+      }else{
+        var id = obj.id.replace(/[^0-9]/g, '');
+      }
+
+      //datos y conexion a la tabla de vehiculo, solo si el auto participo en la multa
+      if (obj.vehiculo == 'si' && validator.isNumeric(obj.matricula)){
+        var id_vehi = obj.matricula;
+        var data_vehi = {matricula_vehiculo: obj.matricula, modelo_vehiculo: obj.modelo, marca_vehiculo: obj.marca, fecha_matricula: obj.date_vehi, id_propietario: id};
+        ins2 = 'INSERT INTO Vehiculo SET ?';
+        connection.query(ins2,data_vehi,function(err, res){
+          if(err){
+            throw err;
+          }
+          console.log("insercion completa en vehiculo");
+        });
+      } else{
+        var id_vehi = null;
+        var data_vehi = {};
+        console.log("No tuvo que agregar vehiculo ó matricula de vehiculo mal escrito");
+      }
+
+      //datos y conexion para lugar de infraccion y multa
+      var data_lugar_infracion = {carretera_lugarInfraccion: obj.carretera, kilometro_lugarInfraccion: obj.km, dir_lugarInfraccion: obj.dir_mult};
+      ins3 = 'INSERT INTO Lugar_Infraccion SET ?';
+      connection.query(ins3,data_lugar_infracion,function(err, res){
+        if(err){
+          throw err;
+        }
+        console.log("Inserccion completa en lugar de infraccion");
+        var id_lugar = res.insertId;
+        var data_mult = {fecha_multa: obj.date_mult, importe_multa: obj.importe,articulo_multa: obj.articulo, id_persona: id, id_vehiculo: id_vehi, id_lugarInfraccion: id_lugar, id_agente: obj.id_agent };
+        ins4 = 'INSERT INTO Multa SET ?';
+        connection.query(ins4,data_mult,function(err, res){
+          if(err){
+            throw err;
+          }
+          console.log("insercion completa en Multa");
+        });
+      });
+      res.render('register_mult');
+
+    }else {
+      console.log("error al ingresar el id del agente o de la persona");
+      res.render('register_mult');
+    }
+  }
+
+  catch (ex) {
+    console.error("Internal error:" + ex);
+    return next(ex);
+  }
+  });
+
+//Pagina root
+router.post('/root',function(req,res,next){
+  if(req.body.boton == 'Ingresar Multa'){
+    res.redirect('/register_mult');
+  }
+  if(req.body.boton == 'Eliminar Persona'){
+    res.redirect('/delete_person');
+  }
+  if(req.body.boton == 'Actualizar datos de persona'){
+    res.redirect('/update_person');
+  }
+
 });
 
 
